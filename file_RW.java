cbac60557dddea9813634872a074803e764c7eb4
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2021-08-20 04:39:29 +0800

    Update file_RW.java

--- file_RW.java
+++ file_RW.java
@@ -1,19 +1,29 @@
-package Read_Write_file;
+package my_tool;
 
 import java.io.*;
-import java.util.stream.*;
-import java.util.*; 
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.spec.EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class file_RW {
+		
+
+		
 	static void copy(String source_path, String destination_folder) throws IOException {
 		try {
-			int line_number = 0;
+			int line_number;
 			InputStream inputStream = new FileInputStream(source_path);
-			InputStreamReader isr = new InputStreamReader(inputStream,"GBK");
+			InputStreamReader isr = new InputStreamReader(inputStream);
 			BufferedReader bReader = new BufferedReader(isr);
 			FileWriter fWriter = new FileWriter(destination_folder);
 			BufferedWriter bWriter =new BufferedWriter(fWriter);
 			String line;
+			line_number =0;
 			while ((line = bReader.readLine()) != null) {
 //				System.out.println(line.length());
 			//System.out.println(line_number);
@@ -32,13 +42,124 @@ public class file_RW {
 		}
 	}
 
+	public void write_folder_info() {
+		System.out.println("default write_folder_info() constructor");
+	}
+
+	public static int get_line_number(String file_path) throws IOException {
+		FileReader fReader = new FileReader(file_path);
+		BufferedReader bReader = new BufferedReader(fReader);
+		int line_number=0;
+		while ((bReader.readLine())!=null) {
+			line_number ++;
+		}
+		bReader.close();
+		fReader.close();
+		return line_number;
+	}
+	
+	public static void write_folder_info(String path) throws IOException {
+		
+		File file = new File(path);
+		String[] file_liSt = file.list();
+		// write file location
+		String temp_file = "C:\\Users\\tiger\\Desktop\\temp\\";
+//		filename
+		String file_name = file.getName().concat("_before_soting");
+		String document_extension = file_name.concat(".txt");
+		String full_destination = temp_file.concat(document_extension);
+		// write file's txt file
+		FileWriter fileWriter = new FileWriter(full_destination);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		boolean filecanread = file.canRead();
+		boolean isdir = file.isDirectory();
+		if (filecanread && isdir) {
+			for (int index = 0; index < file.listFiles().length; index++) {
+				// for cast
+				
+				File f = new File(file_liSt[index]);
+				String string = file_name.toString();
+//				System.out.println(f.isDirectory());
+				System.out.println(index);
+
+				bufferedWriter.write(string);
+				bufferedWriter.write("NO");
+				bufferedWriter.write(Integer.toString(index + 1));
+				bufferedWriter.write(",");
+				bufferedWriter.write(file_liSt[index]);
+
+				String file_full_name = path.concat(file_liSt[index]);
+				Path path_one_file = Paths.get(file_full_name);
+				Long file_size = Files.size(path_one_file);
+
+				bufferedWriter.write("," + file_size + " bits");
+				bufferedWriter.newLine();
+
+			}
+			System.out.println("write file place:");
+			System.out.println(full_destination);
+			bufferedWriter.close();
+			fileWriter.close();
+		} else {
+			System.out.println("file canread :"+filecanread);
+			System.out.println("is directory :"+isdir);
+		}
+
+	}
+	
+	static void print_to_screen() {
+		System.out.println("print_to_screen default constructor");
+	}
+	public static void print_to_screen(String file_path,int columns,String charset_name ) throws IOException {	
+		try {
+			int line_number = 0;
+			InputStream inputStream = new FileInputStream(file_path);
+			InputStreamReader isr = new InputStreamReader(inputStream,charset_name);
+			BufferedReader bReader = new BufferedReader(isr);
+			String line;
+		//	System.out.println("encode="+fReader.getEncoding());
+			while ((line = bReader.readLine()) != null) {
+				line_number++;
+				if(line_number<=columns) {
+					System.out.println("line_number : "+line_number);
+					System.out.println(line);
+				}
+			}
+			bReader.close();
+			isr.close();
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	static void print_to_screen(String file_path,int columns) throws IOException {
+		try {
+			int line_number = 0;
+			FileReader fReader = new FileReader(file_path);
+			BufferedReader bReader = new BufferedReader(fReader);
+			String line;
+		//	System.out.println("encode="+fReader.getEncoding());
+			while ((line = bReader.readLine()) != null) {
+				line_number++;
+				if(line_number<=columns) {
+					System.out.println(line_number);
+					System.out.println(line);
+				}
+			}
+			bReader.close();
+			fReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	static void print_to_screen(String file_path) throws IOException {
 		try {
 			int line_number = 0;
 			FileReader fReader = new FileReader(file_path);
 			BufferedReader bReader = new BufferedReader(fReader);
 			String line;
-			System.out.println("encode="+fReader.getEncoding());
+			//System.out.println("encode="+fReader.getEncoding());
 			while ((line = bReader.readLine()) != null) {
 				line_number++;
 				System.out.println(line_number);
@@ -50,15 +171,21 @@ public class file_RW {
 			e.printStackTrace();
 		}
 	}
+	
 //look specified file 
-	static void Show_file_info(String file_path) {
+	static String[] get_file_list(String file_path) {
 		File file = new File(file_path);
-		System.out.println("canExecute=" + file.canExecute());
-		System.out.println("canRead=" + file.canRead());
-		System.out.println("canwrite=" + file.canWrite());
-		System.out.println("exit=" + file.exists());
-	}
+		String[] file_member = file.list();
+		int numer_of_files = file.listFiles().length;
+		String [] full_path = new String[numer_of_files];
 
+			for (int i=0 ;i<file.list().length;i++) {
+				full_path[i] = file_path+file_member[i];
+			}
+					
+		return full_path;
+	}
+	
 	static void remove_string(String source_path,String destination_folder) throws IOException {
 		try {
 			int line_number = 0;
@@ -74,39 +201,7 @@ public class file_RW {
 			BufferedWriter bWriter = new BufferedWriter(osr);
 			String line;
 			
-			int[] err_code = {37810,65533,32,22,00,2,16,28,37804,12,5,1,25,24,20,18,17,7,8,3,4,19,6,23,21,9};
-			String unknow_word="凢圠怭俙棐揁憖丗儗慞榯嘝杚厱厡檉慺侾憒攓厜枑晫攖慤杸厊抃";
-
-			//per line
-			while((line=bReader.readLine())!=null){
-					ArrayList arraylist = new ArrayList();
-				//per char
-				for(int i=0;i<line.length();i++){
-					ch = line.charAt(i);
-					ascii_num = (int)ch;
-					
-					
-					for(int temp=0;temp<err_code.length;temp++){
-						arraylist.add(err_code[temp]);
-					}
-					boolean is_err_code = arraylist.contains(ascii_num);
-					
-					String cstring = Character.toString(ch);
-					boolean is_unknow_word = unknow_word.contains(cstring);
-						
-					
-					if(is_err_code||is_unknow_word){
-						//System.out.print("1");
-						//System.out.print(is_unknow_word);
-						//bWriter.write(" ");
-					}else{
-						//System.out.println("2");
-						bWriter.write(ch);
-					}
-				}
-				bWriter.newLine();
-			}
-			bReader.close();
+ 			bReader.close();
 			inputStream.close();
 			isr.close();
 			bWriter.close();
@@ -116,4 +211,40 @@ public class file_RW {
 			e.printStackTrace();
 		}
 	}
+	
+		
+	
+	
+	static ArrayList<Integer> indexofstring(String line_string ,char want_find_ch){
+		int line_length =line_string.length();
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for (int index=0;index<line_length;index++) {
+			if(line_string.charAt(index)==want_find_ch) {
+				arrayList.add(index);
+//				System.out.println(line_string.charAt(i));
+			}
+		}
+		return arrayList;
+	}
+	
+	public static ArrayList<String> noduplicatestring(ArrayList<String> datastringArrayList ){
+		Collections.sort(datastringArrayList);
+		ArrayList<String> noduplicateArrayList=new ArrayList<>();
+		for(int i=0;i<datastringArrayList.size();i++) {
+			if(i+1<datastringArrayList.size()) { 
+			  String currentString = datastringArrayList.get(i);
+			  String nexString = datastringArrayList.get(i+1);
+			   if( nexString.equals(currentString)) {
+//				   System.out.println(i+","+"currentString:"+currentString+",");
+//				   System.out.println(i+1+","+"nexString:"+nexString);
+			   } else {
+				   noduplicateArrayList.add(currentString);
+			   }
+			 }else {
+				noduplicateArrayList.add(datastringArrayList.get(i));
+			}
+		}
+		return noduplicateArrayList;
+	}
+	
 }
